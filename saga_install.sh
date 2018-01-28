18613054a206d24eac0be54dd3eee9c1050121b7
Josh <paranoidtruth@yahoo.com>
2018-01-28 12:00:04 -0500

    fix swap

--- saga_install.sh
+++ saga_install.sh
@@ -24,11 +24,13 @@ echo -n "Installing with GENKEY: $GENKEY, RPC PASS: $PASSWORD, VPS IP: $WANIP...
 #begin optional swap section
 echo "Setting up disk swap..."
 free -h 
-sudo fallocate -l 4G /swapfile ls -lh /swapfile
+sudo fallocate -l 4G /swapfile 
+ls -lh /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab sudo bash -c "echo 'vm.swappiness = 10' >> /etc/sysctl.conf"
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab sudo bash -c "
+echo 'vm.swappiness = 10' >> /etc/sysctl.conf"
 free -h
 echo "SWAP setup complete..."
 #end optional swap section
